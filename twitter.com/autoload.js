ce6976025748fcbf952749a446ba2f070aa7412a
Nathan Zadoks <nathan@nathan7.eu>
2013-02-16 10:52:37 +0100

    awesomer scroll behaviour

--- twitter.com/autoload.js
+++ twitter.com/autoload.js
@@ -2,5 +2,7 @@ setInterval(function() {
   if (window.scrollY) return
   var bar = document.querySelector('.new-tweets-bar')
   if (!bar) return
+  var tweet = document.querySelector('.stream-item[data-item-type=tweet]')
   bar.click()
+  window.scrollTo(window.scrollX, tweet.getBoundingClientRect().top + window.scrollY - 50)
 }, 1000)
