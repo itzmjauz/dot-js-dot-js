0042647ebb0830b1cf03e7d500a2a6bd72a2b359
Nathan Zadoks <nathan@nathan7.eu>
2013-02-16 10:52:49 +0100

    formatting

--- twitter.com/autoload.js
+++ twitter.com/autoload.js
@@ -1,7 +1,9 @@
 setInterval(function() {
   if (window.scrollY) return
+
   var bar = document.querySelector('.new-tweets-bar')
   if (!bar) return
+
   var tweet = document.querySelector('.stream-item[data-item-type=tweet]')
   bar.click()
   window.scrollTo(window.scrollX, tweet.getBoundingClientRect().top + window.scrollY - 50)
